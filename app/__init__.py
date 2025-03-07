e43f421a7834db594c275f487ab4ea6a30eaa301
Shamari McPherson <mcphersonshamari1011@gmail.com>
2025-03-07 10:23:46 -0500

    Fixed Flask-Migrate setup and initialized migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,28 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .config import Config
-# import flask migrate here
+import os
+from dotenv import load_dotenv
 
+# Load environment variables
+load_dotenv()
+
+# Create Flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# Initialize database
 db = SQLAlchemy(app)
-# Instantiate Flask-Migrate library here
 
-# Flask-Login login manager
+# Initialize Flask-Migrate here
+migrate = Migrate(app, db)
+
+# Flask-Login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-from app import views
+# Import views and models AFTER initializing app, db, and migrate
+from app import views, models
--- app/models.py
+++ app/models.py
@@ -1,11 +1,6 @@
-from . import db
-
+from app import db  # Import db properly
 
 class UserProfile(db.Model):
-    # You can use this to change the table name. The default convention is to use
-    # the class name. In this case a class name of UserProfile would create a
-    # user_profile (singular) table, but if we specify __tablename__ we can change it
-    # to `user_profiles` (plural) or some other name.
     __tablename__ = 'user_profiles'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -29,4 +24,4 @@ class UserProfile(db.Model):
             return str(self.id)  # python 3 support
 
     def __repr__(self):
-        return '<User %r>' % (self.username)
+        return f'<User {self.username}>'
